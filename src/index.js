57daab696de816b6e00b09c38d4c79fc6bb02e8e
danmj <daniel.jung@stern.nyu.edu>
2018-10-26 09:18:15 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,125 +1,90 @@
-/// DID NOT KNOW WHAT TO DO WITH THE /likes/, /comments/ LINKS......... README DID NOT EVEN MENTION IT..........................................
-
-// COULD NOT FINISH BACKEND POSTING AS A RESULT......................
-
-
-
-
-
-
-
-
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1198 //Enter the id from the fetched image here
-
-  const imageURL = `https://randopic.herokuapp.com/images/1198`
-
+  let imageId = 1296 //Enter the id from the fetched image here
+  const imageURL = `https://randopic.herokuapp.com/images/1296`
   const likeURL = `https://randopic.herokuapp.com/likes/`
-
   const commentsURL = `https://randopic.herokuapp.com/comments/`
-
   const imageLocation = document.getElementById('image')
-
   const imageName = document.getElementById('name')
-
   let imageLikes = document.getElementById('likes')
-
   const likeButton = document.getElementById('like_button')
-
   const commentForm = document.getElementById('comment_form')
-
   const commentList = document.getElementById('comments')
-
   const commentInput = document.getElementById('comment_input')
-
 
   displayImage()
   function displayImage() {
-    fetch(imageURL)
+    fetch('https://randopic.herokuapp.com/images/1296')
       .then(res => res.json())
       .then(image => {
         imageLocation.src = image.url
         imageName.innerText = image.name
-        imageLikes.innerText = `${image.like_count}`
+        imageLikes.innerText = image.like_count
         image.comments.forEach((comment) => {
-          commentList.innerText = `${comment.content}`
+          displayComment(comment.content)
         })
       })
+
+      res => res.json() => res.json().url or res.json().name
   } // END OF function displayImage()
 
 
+  likeButton.addEventListener('click', (event) => {
+    like()
+  })
 
-  listenForLikes()
-  function listenForLikes() {
-    likeButton.addEventListener('click', (event) => {
-      let likeCount = parseInt(imageLikes.innerText)
-      ++likeCount
-      imageLikes.innerText = likeCount
+  function like() {
+    // FRONTEND
+    let likeCount = parseInt(imageLikes.innerText)
+    ++likeCount
+    imageLikes.innerText = likeCount
+
+    //BACKEND
+    fetch('https://randopic.herokuapp.com/likes/', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        image_id: 1296,
+        likes: likeCount
+      })
     })
 
-  } // END OF function listenForLikes()
+  } // END OF function like()
 
 
-  // fetch(likeURL, {
-  //   method: "PATCH",
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     'Accept': 'application/json'
-  //   },
-  //   body: JSON.stringify({
-  //     image_id: 1198
-  //   })
-  // })
-  //   .then(res => res.json())
-  //   .then()
+  commentForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    inputComment()
+  })
 
-
-
-
-
-
-
-
-  inputComment()
   function inputComment() {
-    commentForm.addEventListener('submit', (event) => {
-      event.preventDefault()
-      let commentLi = document.createElement('li')
-      commentLi.innerText = `${commentInput.value}`
-      commentList.appendChild(commentLi)
-      commentForm.reset()
+    displayComment(commentInput.value)
+
+    fetch('https://randopic.herokuapp.com/comments/', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        image_id: 1296,
+        content: commentInput.value
+      })
     })
 
+    commentInput.value = ''
 
+  } // END OF function inputComment
 
-  //   fetch(commentsURL, {
-  //     method: 'PATCH',
-  //     headers: {
-  //       'Accept': 'application/json',
-  //       'Content-Type': 'application/json'
-  //     },
-  //     body: JSON.stringify({
-  //       image_id: 1198,
-  //       content: commentInput.value
-  //     })
-  //   })
-  //   .then()
-  // } END OF function inputComment()
-
-
-
-  function commentSubmitHandler() {
-
+  function displayComment(str) {
+    let commentLi = document.createElement('li')
+    commentLi.innerText = str
+    commentList.appendChild(commentLi)
   }
-
-
-
-
-
-
-
 
 
 }) // END OF DOMContentLoaded
